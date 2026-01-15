685c5b4db8d6102644d370734d1d1e23beed8f9d
anasbughio <anasbughio@gmail.com>
2026-01-15 05:44:04 +0500

    buypoints

--- src/pages/BuyPoints.jsx
+++ src/pages/BuyPoints.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { supabase } from '../supabaseClient';
-import '../CSS/buypoints.css';
+import '../CSS/buyPoints.css';
 
 const BuyPoints = () => {
   const [loading, setLoading] = useState(false);
